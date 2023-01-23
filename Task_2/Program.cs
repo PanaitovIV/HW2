d149f01c129a226aee337da9069a4e3ecb571c6d
Ivan Panaitov <PanaitovIV@gmail.com>
2023-01-23 18:07:40 +0300

    Задание второе

---
+++ Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+int InputInt(string message)
+{
+    System.Console.Write(message + " > ");
+    string? inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+bool FindThirdNumber(int number)
+{
+	if(number > 99)
+    {
+        return true;
+    }
+    System.Console.WriteLine("В данном числе нет третьей цифры!");
+	return false;
+}
+
+int number = InputInt("Введите любое целое число");
+if (FindThirdNumber(number))
+{
+    string numbers = number.ToString();
+    char thirdNumber = numbers[2];
+    System.Console.WriteLine($"В введенном числе {number} третьей цифрой является > {thirdNumber}");
+}
